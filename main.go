0aeb01f98e805120d125149ac430f5a126b24627
ichiz7 <ichibakry7@gmail.com>
2024-09-12 16:35:16 +0200

    add getURLsFromHTML function

--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	normalizedURL, err := normalizeURL("https://blog.boot.dev/path%20with%20spaces")
 
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("Normalized URL:", normalizedURL)
-	}
 }
--- normalize_url.go
+++ normalize_url.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"net/url"
 	"strings"
+
+	"golang.org/x/net/html"
 )
 
 func normalizeURL(rawURl string) (string, error) {
@@ -22,4 +25,39 @@ func normalizeURL(rawURl string) (string, error) {
 	}
 
 	return strings.ToLower(normalized), nil
+}
+
+func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+	doc, err := html.Parse(strings.NewReader(htmlBody))
+	if err != nil {
+		return nil, err
+	}
+
+	base, err := url.Parse(rawBaseURL)
+	if err != nil || !base.IsAbs() {
+		return nil, errors.New("invalid base url")
+	}
+
+	var urls []string
+	var f func(n *html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, attr := range n.Attr {
+				if attr.Key == "href" {
+					href, err := base.Parse(attr.Val)
+					if err == nil {
+						urls = append(urls, href.String())
+					}
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(doc)
+
+	return urls, nil
+
 }
